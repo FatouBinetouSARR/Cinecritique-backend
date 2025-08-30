3b375bbb9f64c918feff0d65b4a06b3f288ee4c8
Adam01-i <seckmote@gmail.com>
2025-08-30 12:22:02 +0000

    fix error deploy

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -6,9 +6,6 @@ const {
   updateReview,
   deleteReview,
   getMyReviews,
-  getTopRatedMovies,
-  toggleLikeReview,
-  checkUserLike,
 } = require("../controllers/reviewController.js");
 
 const router = express.Router();
@@ -23,12 +20,5 @@ router.delete("/movies/:movieId/reviews/:id", authenticate, deleteReview);
 
 // (optionnel) Mes critiques
 router.get("/reviews/mine", authenticate, getMyReviews);
-
-// Classement des films les mieux notés (agrégés depuis les critiques)
-router.get("/reviews/top-rated", getTopRatedMovies);
-
-// Gestion des likes
-router.post("/reviews/:id/like", authenticate, toggleLikeReview);
-router.get("/reviews/:id/like", authenticate, checkUserLike);
 
 module.exports = router;
